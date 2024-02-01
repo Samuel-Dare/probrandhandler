4572e5337961820e86a424032e138677c73a9f0b
Samuel Oluwadare <oluwadaresamuel39@gmail.com>
2024-02-01 10:07:02 +0100

    completed gh-pages settings

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ command, mode }) => {
-  let baseUrl;
-
-  if (mode === 'development') {
-    baseUrl = 'http://localhost:3000';
-  } else if (mode === 'production') {
-    baseUrl = 'https://www.probrandhandlers.com';
-  }
-
-  return {
-    base: '/probrandhandlers/',
-    plugins: [react()],
-    define: {
-      'import.meta.env.VITE_BASE_URL': JSON.stringify(baseUrl),
-    },
-  };
+export default defineConfig({
+  base: '/probrandhandlers/',
+  plugins: [react()],
 });
